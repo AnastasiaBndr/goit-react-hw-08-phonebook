771b0b008496723103fc7152f06283a22c9397d9
AnastasiaBndr <125996065+AnastasiaBndr@users.noreply.github.com>
2023-10-19 13:59:37 +0000

    Deploying to gh-pages from @ AnastasiaBndr/goit-react-hw-08-phonebook@8168871e4c25039c403b9e9964ae014a3e22a28b 🚀

---
+++ static/js/513.19589266.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[513],{4513:function(e,n,r){r.r(n),r.d(n,{default:function(){return c}});var t=r(184),c=function(){return(0,t.jsxs)("div",{className:"welcome-text container",children:[(0,t.jsx)("h1",{children:"Welcome to your phone page!"}),(0,t.jsx)("p",{className:"custom-message",children:"Enjoy your browsing experience!"})]})}}}]);
+//# sourceMappingURL=513.19589266.chunk.js.map
